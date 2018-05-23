4bd530b688f5dcd71f9b434e71c90e5337ed70bb
Frank de Jong <frank.049@hotmail.com>
2018-05-23 22:40:34 +0200

    Add initial dotfiles and bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+export DOTFILES={$HOME}/dev/dotfiles
+# Remove old config dirs and files.
+cd
+rm -rf {$DOTFILES} .config/nvim/ .config/alacritty/
+rm -rf .tmux.conf
+
+# Clone dotfiles repo.
+git clone --quiet https://github.com/mauroporrasp/dotfiles.git
+
+# Create new symlinks.
+mkdir -p .config/nvim/
+ln -s {$DOTFILES}/vim/init.vim ~/.config/nvim/init.vim
+
+mkdir -p .config/alacritty/
+ln -s {$DOTFILES}/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+
+ln -s {$DOTFILES}/tmux.conf ~/.tmux.conf
+
+# Start Neovim.
+echo 'Starting Neovim'
