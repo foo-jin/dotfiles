7e9a5086c1dc4dc7cc7439aabcdc9abae9a14819
Frank de Jong <frank.049@hotmail.com>
2018-05-23 22:55:03 +0200

    Fix bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
-export DOTFILES={$HOME}/dev/dotfiles
+export DOTFILES=$HOME/dev/dotfiles
 # Remove old config dirs and files.
 cd
-rm -rf {$DOTFILES} .config/nvim/ .config/alacritty/
+rm -rf $DOTFILES .config/nvim/ .config/alacritty/
 rm -rf .tmux.conf
 
 # Clone dotfiles repo.
@@ -11,12 +11,13 @@ git clone --quiet https://github.com/mauroporrasp/dotfiles.git
 
 # Create new symlinks.
 mkdir -p .config/nvim/
-ln -s {$DOTFILES}/vim/init.vim ~/.config/nvim/init.vim
+ln -s $DOTFILES/vim/init.vim ~/.config/nvim/init.vim
 
 mkdir -p .config/alacritty/
-ln -s {$DOTFILES}/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -s $DOTFILES/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 
-ln -s {$DOTFILES}/tmux.conf ~/.tmux.conf
+ln -s $DOTFILES/tmux.conf ~/.tmux.conf
 
 # Start Neovim.
 echo 'Starting Neovim'
+nvim +PluginInstall +UpdateRemotePlugins +qa
