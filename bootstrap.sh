42e91f83dd840629f21ae17e7d7e330d4ee08d00
frank <frank.049@hotmail.com>
2018-11-12 19:57:41 +0100

    Add fish config and update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 script_home="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Update the dotfiles repo.
-git pull
+git pull --rebase
 
 # Create new symlinks.
 ln -sf $script_home/tmux/tmux.conf $HOME/.tmux.conf
@@ -13,5 +13,7 @@ ln -sf $script_home/vim/init.vim $HOME/.config/nvim/init.vim
 
 mkdir -p $HOME/.config/alacritty/
 ln -sf $script_home/alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml
+
+ln -s ~/dev/software/dotfiles/fish ~/.config/fish
 
 nvim +PluginInstall +UpdateRemotePlugins +qa
---
+++ fish/config.fish
@@ -0,0 +1,90 @@
+set -U fish_user_abbreviations
+set -U fish_user_abbreviations $fish_user_abbreviations 'c=cargo'
+set -U fish_user_abbreviations $fish_user_abbreviations 'm=make'
+set -U fish_user_abbreviations $fish_user_abbreviations 'o=xdg-open'
+set -U fish_user_abbreviations $fish_user_abbreviations 'g=git'
+set -U fish_user_abbreviations $fish_user_abbreviations 'gc=git checkout'
+set -U fish_user_abbreviations $fish_user_abbreviations 'vimdiff=nvim -d'
+set -U fish_user_abbreviations $fish_user_abbreviations 'gah=git stash; and git pull --rebase; and git stash pop'
+complete --command yay --wraps pacman
+
+if status --is-interactive
+    tmux ^ /dev/null; and exec true
+end
+
+if exa --version >/dev/null
+    set -U fish_user_abbreviations $fish_user_abbreviations 'l=exa'
+    set -U fish_user_abbreviations $fish_user_abbreviations 'ls=exa -l'
+    set -U fish_user_abbreviations $fish_user_abbreviations 'lls=exa -la'
+else
+    set -U fish_user_abbreviations $fish_user_abbreviations 'l=ls'
+    set -U fish_user_abbreviations $fish_user_abbreviations 'ls=ls -l'
+    set -U fish_user_abbreviations $fish_user_abbreviations 'lls=ls -la'
+end
+
+function remote_alacritty
+    # https://gist.github.com/costis/5135502
+    set fn (mktemp)
+    infocmp alacritty-256color > $fn
+    scp $fn $argv[1]":alacritty-256color.ti"
+    ssh $argv[1] tic "alacritty-256color.ti"
+    ssh $argv[1] rm "alacritty-256color.ti"
+end
+
+set PATH /usr/local/bin/ $PATH
+set PATH $PATH ~/bin
+set PATH $PATH ~/.cargo/bin
+
+setenv EDITOR nvim
+setenv BROWSER firefox
+setenv TZ 'Europe/Amsterdam'
+setenv RUST_BACKTRACE 1
+setenv RUSTFLAGS "-C target-cpu=native"
+
+function fish_greeting
+    echo
+    echo -e (uname -ro | awk '{print " \\\\e[1mOS: \\\\e[0;32m"$0"\\\\e[0m"}')
+    echo -e (uptime -p | sed 's/^up //' | awk '{print " \\\\e[1mUptime: \\\\e[0;32m"$0"\\\\e[0m"}')
+    echo -e (uname -n | awk '{print " \\\\e[1mHostname: \\\\e[0;32m"$0"\\\\e[0m"}')
+    echo -e " \\e[1mDisk usage:\\e[0m"
+    # echo
+    echo -ne (\
+        df -l -h | grep -E 'dev/(xvda|sd|mapper)' | \
+        awk '{printf "\\\\t%s\\\\t%4s / %4s  %s\\\\n\n", $6, $3, $2, $5}' | \
+        sed -e 's/^\(.*\([8][5-9]\|[9][0-9]\)%.*\)$/\\\\e[0;31m\1\\\\e[0m/' -e 's/^\(.*\([7][5-9]\|[8][0-4]\)%.*\)$/\\\\e[0;33m\1\\\\e[0m/' | \
+        paste -sd ''\
+    )
+    # echo
+
+    echo -e " \\e[1mNetwork:\\e[0m"
+    # echo
+    # http://tdt.rocks/linux_network_interface_naming.html
+    echo -ne (\
+        ip addr show up scope global | \
+            grep -E ': <|inet' | \
+            sed \
+                -e 's/^[[:digit:]]\+: //' \
+                -e 's/: <.*//' \
+                -e 's/.*inet[[:digit:]]* //' \
+                -e 's/\/.*//'| \
+            awk 'BEGIN {i=""} /\.|:/ {print i" "$0"\\\n"; next} // {i = $0}' | \
+            sort | \
+            column -t -R1 | \
+            # public addresses are underlined for visibility \
+            sed 's/ \([^ ]\+\)$/ \\\e[4m\1/' | \
+            # private addresses are not \
+            sed 's/m\(\(10\.\|172\.\(1[6-9]\|2[0-9]\|3[01]\)\|192\.168\.\).*\)/m\\\e[24m\1/' | \
+            # unknown interfaces are cyan \
+            sed 's/^\( *[^ ]\+\)/\\\e[36m\1/' | \
+            # ethernet interfaces are normal \
+            sed 's/\(\(en\|em\|eth\)[^ ]* .*\)/\\\e[39m\1/' | \
+            # wireless interfaces are purple \
+            sed 's/\(wl[^ ]* .*\)/\\\e[35m\1/' | \
+            # wwan interfaces are yellow \
+            sed 's/\(ww[^ ]* .*\).*/\\\e[33m\1/' | \
+            sed 's/$/\\\e[0m/' | \
+            sed 's/^/\t/' \
+    )
+    set_color normal
+    echo
+end
