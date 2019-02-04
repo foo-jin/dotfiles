ebdd78e07a6c819d6d5423646451c19aaaf18697
frank <frank.049@hotmail.com>
2019-02-04 15:41:29 +0100

    Add ssh-agent autostarting to init.fish

--- fish/config.fish
+++ fish/config.fish
@@ -62,6 +62,35 @@ if test -e $localcfg
     source $localcfg
 end
 
+# auto-start ssh agent
+# https://github.com/danhper/fish-ssh-agent
+function ssh_agent_is_started -d "check if ssh agent is already started"
+  	if begin; test -f $SSH_ENV; and test -z "$SSH_AGENT_PID"; end
+		source $SSH_ENV > /dev/null
+	end
+
+	if test -z "$SSH_AGENT_PID"
+		return 1
+	end
+
+	ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep -q ssh-agent
+	return $status
+end
+
+function ssh_agent_start -d "start a new ssh agent"
+  ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
+  chmod 600 $SSH_ENV
+  source $SSH_ENV > /dev/null
+end
+
+if test -z "$SSH_ENV"
+    set -xg SSH_ENV $HOME/.ssh/environment
+end
+
+if not ssh_agent_is_started
+    ssh_agent_start
+end
+
 function fish_greeting
     echo
     echo -e (uname -ro | awk '{print " \\\\e[1mOS: \\\\e[0;32m"$0"\\\\e[0m"}')
